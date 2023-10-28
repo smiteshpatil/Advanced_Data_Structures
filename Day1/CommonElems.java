30a2c5e72c895b2b240f60b9603624ee148c8ee5
Smitesh Patil <59860322+smiteshpatil@users.noreply.github.com>
2023-10-28 19:46:50 +0530

    Add files via upload

---
+++ Day1/CommonElems.java
@@ -0,0 +1,57 @@
+package dsa;
+
+import java.util.ArrayList;
+
+public class CommonElems {
+
+	// main method
+	public static void main(String[] args) {
+
+		int[] arr1 = { 1, 3, 4, 6, 7, 9 };
+		int[] arr2 = { 1, 2, 4, 5, 9, 10 };
+
+//		ArrayList<Integer> commonElems = findCommonElems(arr1, arr2);
+		ArrayList<Integer> commonElems = findCommonElemsOptimized(arr1, arr2);
+
+		for (int i : commonElems)
+			System.out.print(i + " ");
+
+	}
+
+	// function to find common elements
+	// O(n^2)
+	public static ArrayList<Integer> findCommonElems(int[] a1, int[] a2) {
+
+		ArrayList<Integer> common = new ArrayList<Integer>();
+
+		for (int i = 0; i < a1.length; i++) {
+			for (int j = 0; j < a2.length; j++) {
+				if (a1[i] == a2[j])
+					common.add(a1[i]);
+			}
+		}
+		return common;
+	}
+
+	
+	// function to find common elements (both arrays must be sorted)
+	// two pointer approach
+	public static ArrayList<Integer> findCommonElemsOptimized(int[] a1, int[] a2) {
+
+		ArrayList<Integer> common = new ArrayList<Integer>();
+
+		int i = 0, j = 0;
+
+		while (i != a1.length && j != a2.length) {
+			
+			if (a1[i] == a2[j])
+				common.add(a1[i]);
+			else if (a1[i] < a2[j])
+				i++;
+			else
+				j++;
+		}
+
+		return common;
+	}
+}
---
+++ Day1/MostFreqOccuring.java
@@ -0,0 +1,71 @@
+package dsa;
+
+import java.util.HashMap;
+
+public class MostFreqOccuring {
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 3, 1, 3, 2, 1, 4, 4, 4, 4, 4, 4 };
+
+//		Integer mostFreqEle = findMostFreqEle(array);
+		Integer mostFreqEle = findMostFreqUsingHM(array);
+		System.out.println(mostFreqEle);
+	}
+
+// naive solution O(n^2)
+	public static Integer findMostFreqEle(int[] array) {
+
+		// if array is empty return null
+		if (array.length == 0)
+			return null;
+
+		int mostFreqEle = 0, prevCount = 0, currCount = 0;
+		;
+
+		for (int i = 0; i < array.length; i++) {
+
+			for (int j = i + 1; j < array.length - 1; j++) {
+
+				if (array[i] == array[j])
+					currCount++;
+			}
+
+			if (currCount > prevCount) {
+				mostFreqEle = array[i];
+				prevCount = currCount;
+			}
+
+		}
+
+		return mostFreqEle;
+	}
+
+	
+	
+// optimized using hashMap  O(n)
+
+	public static Integer findMostFreqUsingHM(int[] array) {
+
+		int mostFreqEle = 0, count = 0;
+
+		if (array.length == 0)
+			return null;
+
+		// create a hashMap key(array element) value => count
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i : array) {
+			if (map.putIfAbsent(i, 1) != null) {
+				map.put(i, map.get(i) + 1);
+			}
+			if (map.get(i) > count) {
+
+				mostFreqEle = i;
+				count = map.get(i);
+			}
+		}
+
+		return mostFreqEle;
+	}
+}
